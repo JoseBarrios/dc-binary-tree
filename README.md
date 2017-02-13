346bf155bf4e0d13b0f170dfdc1c9ce543d99bd6
Jose Barrios <jose@cognilab.com>
2017-02-13 14:23:05 -0800

    Updated README with basic functionality (more coming). Minor unit tests cleanup

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # Usage
 
+#### 1. Building the tree:
+
+```javascript
 var BinaryTree = require('binarytree');
 
-var wikiTree = new BinaryTree('F')
-wikiTree.insertLeft('B');
-wikiTree.left.insertLeft('A');
-wikiTree.left.insertRight('D');
-wikiTree.left.right.insertLeft('C');
-wikiTree.left.right.insertRight('E');
-wikiTree.insertRight('G');
-wikiTree.right.insertRight('I');
-wikiTree.right.right.insertLeft('H');
+var binaryTree = new BinaryTree('F');
+let bNode = binaryTree.appendChildNode('B', 'left', alphabetTree.root);
+let aNode = binaryTree.appendChildNode('A', 'left', bNode);
+let dNode = binaryTree.appendChildNode('D', 'right', bNode);
+let cNode = binaryTree.appendChildNode('C', 'left', dNode);
+let eNode = binaryTree.appendChildNode('E', 'right', dNode);
+let gNode = binaryTree.appendChildNode('G', 'right', binaryTree.root);      
+let iNode = binaryTree.appendChildNode('I', 'right', gNode);                 
+let hNode = binaryTree.appendChildNode('H', 'left', iNode);   
+```
+![image](https://upload.wikimedia.org/wikipedia/commons/d/d4/Sorted_binary_tree_preorder.svg)
+
+#### 2. Transversing the tree:
+```javascript
+// Breath Search First
+let breathTransversal = binaryTree.BSF(); //['F','B','G','A','D','I','C','E','H'];    
+
+// Depth Search First
+let preTransversal = binaryTree.DSF('preorder');//['F','B','A','D','C','E','G','I','H'];             
+let inorderTransversal = binaryTree.DSF('inorder'); //['A','B','C','D','E','F','G','H','I'];              
+let postorderTransversal = binaryTree.DSF('postorder');//['A','C','E','D','B','H','I','G','F'];   
+   
+```
--- tests/binaryTreeTests.js
+++ tests/binaryTreeTests.js
@@ -21,15 +21,14 @@ let iNode = alphabetTree.appendChildNode('I', 'right', gNode);
 let hNode = alphabetTree.appendChildNode('H', 'left', iNode);
 
 //Correct outputs
+const CORRECT_A_PATH = ['F','B'];
+const CORRECT_E_PATH = ['F','B','D'];
+const CORRECT_H_PATH = ['F','G','I'];
+const CORRECT_LEAF_DEPTHS = [ 2, 3, 3, 3 ];
+const CORRECT_BSF = ['F','B','G','A','D','I','C','E','H'];
 const CORRECT_DSF_PREORDER = ['F','B','A','D','C','E','G','I','H'];
 const CORRECT_DSF_INORDER = ['A','B','C','D','E','F','G','H','I'];
 const CORRECT_DSF_POSTORDER = ['A','C','E','D','B','H','I','G','F'];
-const CORRECT_BSF = ['F','B','G','A','D','I','C','E','H'];
-const CORRECT_LEAF_DEPTHS = [ 2, 3, 3, 3 ];
-const CORRECT_A_PATH = ['F','B'];
-const CORRECT_A1_PATH = ['F','B', 'A'];
-const CORRECT_E_PATH = ['F','B','D'];
-const CORRECT_H_PATH = ['F','G','I'];
 
 
 describe('BinaryTree', function() {
@@ -56,21 +55,18 @@ describe('BinaryTree', function() {
   describe('#getNodeByValue()', function() {
     it('should return the first instance of the node with matching value', function() {
       assert.deepStrictEqual(alphabetTree.getNodeByValue('A'), aNode );
-      //assert.deepStrictEqual(alphabetTreeWithRepetitions.getNodeByValue('A', 2), a2Node );
     });
   });
 
   describe('#findPathToValue()', function() {
     it('should return the path to the passed value', function() {
       assert.deepStrictEqual(alphabetTree.findPathToValue('A'),CORRECT_A_PATH);
-      //assert.deepStrictEqual(alphabetTreeWithRepetitions.findPathToValue('A', 2),CORRECT_A1_PATH);
     });
   });
 
   describe('#findPathToNode()', function() {
     it('should return the path to the passed value', function() {
       assert.deepStrictEqual(alphabetTree.findPathToNode(aNode),CORRECT_A_PATH);
-      //assert.deepStrictEqual(alphabetTreeWithRepetitions.findPathToNode(a2Node),CORRECT_A1_PATH);
     });
   });
 
