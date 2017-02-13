b71e85a7ae05bf968fa7ce8586770ee3192d0183
Jose Barrios <jose@cognilab.com>
2017-02-13 14:02:11 -0800

    Updated pathfinding and search methods, added test cases

--- index.js
+++ index.js
@@ -9,9 +9,41 @@ class BinaryTree{
     this.root = new BinaryTreeNode(value);
   }
 
+  static print(stack){
+    var values = [];
+    stack.forEach(function(node){
+      values.push(node.value);
+    })
+    console.log(values);
+  }
+
+  getNodeByValue(targetValue, nthMatch){
+    nthMatch = nthMatch  || 1;
+    let targetNode = null;
+    let currentMatches = 0;
+
+    function search(node, value){
+      if(!node)return false;
+      let isMatch = node.value === value;
+      currentMatches += isMatch? 1 : 0;
+      let isNthMatch = currentMatches === nthMatch;
+      if(isMatch && isNthMatch){
+        targetNode = node;
+        return true;
+      }
+      if(search(node.left, value) || search(node.right, value)){
+        return true;
+      }
+    }
+    search(this.root, targetValue)
+    return targetNode;
+  }
+
 
   appendChildNode(value, position, parentNode){
-    position = position.toUpperCase();
+    const DEFAULT_LEAF_POSITION = 'LEFT';
+    position = position.toUpperCase() || DEFAULT_LEAF_POSITION;
+    parentNode = parentNode || this.root;
     switch(position){
       case 'LEFT':
         parentNode.insertLeft(value);
@@ -20,11 +52,191 @@ class BinaryTree{
         parentNode.insertRight(value);
         return parentNode.right;
       default:
-        this.Error(`Invalid position ${position}, arg must be 'left' or 'right'`);
+        console.error(`Invalid position ${position}, arg must be 'left' or 'right'`);
     }
   }
 
 
+  BSF(){
+    var stack = [];
+    var queue = [];
+    queue.push(this.root);
+
+    while(queue.length){
+      //Dequeue node at index 0 (remove from queue)
+      var currentNode = queue.shift();
+      //Add value to stack
+      stack.push(currentNode.value);
+      //Enqueue children (add to queue)
+      if(currentNode.left){ queue.push(currentNode.left) }
+      if(currentNode.right){ queue.push(currentNode.right); }
+    }
+    return stack;
+  }
+
+
+  DSF(transversalOrder){
+    //Most efficient for duplicating trees
+    const PRE = 'PREORDER';
+    //Most commonly used for binary search trees (due to order)
+    const IN = 'INORDER';
+    //Most efficient for deleting or freeing nodes
+    const POST = 'POSTORDER';
+
+    const DEFAULT = IN;
+    var order = [];
+
+    ///////////////////
+    // Validate input
+    ///////////////////
+    transversalOrder = transversalOrder == null? DEFAULT : transversalOrder;
+    let invalidInput = typeof transversalOrder !== 'string';
+    invalidInput = invalidInput || transversalOrder !== PRE;
+    invalidInput = invalidInput && transversalOrder !== IN;
+    invalidInput = invalidInput && transversalOrder !== POST;
+    //If transversal transversalOrder arg is not a string, use default order
+    if(invalidInput){
+      console.warn(`BinaryTree.DSF() argument must be a string of value
+      'PREORDER', 'INORDER', or 'POSTORDER'. You passed '${transversalOrder}'.
+      Defaulting to '${DEFAULT}'`);
+      transversalOrder = DEFAULT;
+    }
+    var transversalOrder = transversalOrder.toUpperCase() || DEFAULT;
+
+    //////////
+    // SEARCH
+    //////////
+    function search(node){
+      if(node === null) return;
+      //PRE: If node is not null, push value to order
+      if(transversalOrder === PRE){ order.push(node.value); }
+      if(node.left){ search(node.left) }
+      //IN: If we've reached leftmost leaf, push value to order
+      if(transversalOrder === IN){ order.push(node.value); }
+      if(node.right){ search(node.right) }
+      //POST: If we've reached left or rightmost leaf, push value to order
+      if(transversalOrder === POST){ order.push(node.value); }
+    }
+    search(this.root);
+    return order;
+  }
+
+
+  getLeafDepths(){
+    var depths = [];
+    function search(node, depth){
+      if(node === null) return;
+      if(!node.left && !node.right){ depths.push(depth); }
+      if(node.left){ search(node.left, depth+1) }
+      if(node.right){ search(node.right, depth+1) }
+    }
+    search(this.root, 0);
+    return depths;
+  }
+
+
+  findPathToNode(target){
+    var ancestors = [];
+    function search(node, target){
+      if(!node)return false;
+      let nodeString = JSON.stringify(node);
+      let targetString = JSON.stringify(target);
+      let deepMatch = nodeString === targetString;
+      if(deepMatch){ return true; }
+      if(search(node.left, target) || search(node.right, target)){
+        ancestors.unshift(node.value);
+        return true;
+      }
+    }
+    search(this.root, target)
+
+    return ancestors.length? ancestors : null;
+  }
+
+
+  //Finds the path to the value provided. If there are multiple values that
+  //match the criteria, the shortest path is returned unless specified in nthMatch
+  findPathToValue(value, nthMatch){
+    var ancestors = [];
+    nthMatch = nthMatch  || 1;
+    let currentMatches = 0;
+    function search(node){
+      //Null nodes cannot be target, return false
+      if(!node)return false;
+
+      let isMatch = node.value === value;
+      if(isMatch){ currentMatches++; }
+      let isTarget = isMatch && currentMatches === nthMatch;
+      if(isTarget){ return true; }
+
+      //If current node's left/right is target value
+      if(search(node.left) || search(node.right)){
+        //Enqueue current node value
+        ancestors.unshift(node.value);
+        return true;
+      }
+    }
+    search(this.root)
+    return ancestors.length? ancestors : null;
+  }
+
+
+  getLeastCommonAncester(a, b){
+    //Check that both a and b exist
+    if(this.findPath(a) && this.findPath(b)){
+      //Iterate in pre-order, searching for a and b
+      function search(node, a, b){
+        //////////////////////
+        // SEARCH RESULTS  //
+        ////////////////////
+        //If incorrect input, return null
+        if(!node || !a || !b){ return null }
+        //If a or b is found, return node reference
+        if(a === node.value || b === node.value){ return node; }
+
+        ////////////////////////////
+        // PRE-ORDER TRASVERSAL  //
+        //////////////////////////
+        //Recurse LEFT nodes (first) until you find a or b
+        var left = search(node.left, a, b);
+        //Recurse RIGHT nodes (when no lefts) until you find a or b
+        var right = search(node.right, a, b);
+
+        //////////////
+        // RESULT  //
+        ////////////
+        //If both left and right found, nodes are in separate branches
+        if(left && right){ return node; }
+        //Otherwise, check if left/right subtree is LCA
+        return left? left : right;
+      }
+      return search(this, a, b).value;
+    }
+    else {return null}
+  }
+
+
+  reverse(){
+    var reversedTree = new BinaryTreeNode(this.value);
+    function search(node, reversedNode){
+      if(node === null){return null}
+      if(node.left){
+        reversedNode.insertRight(node.left.value);
+        search(node.left, reversedNode.right)
+      }
+      if(node.right){
+        reversedNode.insertLeft(node.right.value);
+        search(node.right, reversedNode.left)
+      }
+    }
+    search(this, reversedTree);
+    return reversedTree;
+  }
+
+
+  /////////////////////////////
+  // CONSIDERING REMOVING THESE
+  // //////////////////////////
   singlyLinkedSiblings(){
     var levelOrderNodes = this.BSFWithLevelDelimiter(null);
     for(var i = 0; i < levelOrderNodes.length-1; i++){
@@ -34,6 +246,7 @@ class BinaryTree{
     }
     return levelOrderNodes;
   }
+
 
   BSFWithLevelDelimiter(delimiter){
     var delimiter = delimiter || null;
@@ -53,143 +266,6 @@ class BinaryTree{
     }
     return nodes;
   }
-
-
-  BSF(){
-    var nodes = [];
-    var queue = [];
-    queue.push(this.root);
-    while(queue.length){
-      var currentNode = queue.shift();
-      nodes.push(currentNode.value);
-      if(currentNode.left){ queue.push(currentNode.left) }
-      if(currentNode.right){ queue.push(currentNode.right); }
-    }
-    return nodes;
-  }
-
-  Error(msg){
-    console.warn('ERROR:', msg);
-  }
-
-
-  DSF(transversalOrder){
-    const PRE = 'PREORDER';
-    const IN = 'INORDER';
-    const POST = 'POSTORDER';
-    const DEFAULT = PRE;
-    var nodeArray = [];
-
-    transversalOrder = transversalOrder == null? DEFAULT : transversalOrder;
-    let invalidInput = typeof transversalOrder !== 'string';
-    invalidInput = invalidInput || transversalOrder !== PRE;
-    invalidInput = invalidInput && transversalOrder !== IN;
-    invalidInput = invalidInput && transversalOrder !== POST;
-
-    //If transversal transversalOrder arg is not a string, use default order
-    if(invalidInput){
-      this.Error(`BinaryTree.DSF() argument must be a string of value
-      'PREORDER', 'INORDER', or 'POSTORDER'. You passed '${transversalOrder}'.
-      Defaulting to '${DEFAULT}'`);
-      transversalOrder = DEFAULT;
-    }
-
-    var transversalOrder = transversalOrder.toUpperCase() || DEFAULT;
-
-    function recursiveNodeGetter(node){
-      if(node === null) return;
-      if(transversalOrder === PRE){ nodeArray.push(node.value); }
-      if(node.left){ recursiveNodeGetter(node.left) }
-      if(transversalOrder === IN){ nodeArray.push(node.value); }
-      if(node.right){ recursiveNodeGetter(node.right) }
-      if(transversalOrder === POST){ nodeArray.push(node.value); }
-    }
-    recursiveNodeGetter(this.root);
-
-    return nodeArray;
-  }
-
-
-
-  getDepths(){
-    var depths = [];
-    function recurse(node, depth){
-      if(node === null) return;
-      if(!node.left && !node.right){ depths.push(depth); }
-      if(node.left){ recurse(node.left, depth+1) }
-      if(node.right){ recurse(node.right, depth+1) }
-    }
-    recurse(this, 0);
-    return depths;
-  }
-
-  findPath(value){
-    var ancestors = [];
-    function recurse(node, value){
-      if(!node)return false;
-      if(node.value === value){ return true; }
-      if(recurse(node.left, value) || recurse(node.right, value)){
-        ancestors.unshift(node.value);
-        return true;
-      }
-    }
-    recurse(this, value)
-    return ancestors.length? ancestors : null;
-  }
-
-  getLeastCommonAncester(a, b){
-    //Check that both a and b exist
-    if(this.findPath(a) && this.findPath(b)){
-      //Iterate in pre-order, searching for a and b
-      function recurse(node, a, b){
-        //////////////////////
-        // SEARCH RESULTS  //
-        ////////////////////
-        //If incorrect input, return null
-        if(!node || !a || !b){ return null }
-        //If a or b is found, return node reference
-        if(a === node.value || b === node.value){ return node; }
-
-        ////////////////////////////
-        // PRE-ORDER TRASVERSAL  //
-        //////////////////////////
-        //Recurse LEFT nodes (first) until you find a or b
-        var left = recurse(node.left, a, b);
-        //Recurse RIGHT nodes (when no lefts) until you find a or b
-        var right = recurse(node.right, a, b);
-
-        //////////////
-        // RESULT  //
-        ////////////
-        //If both left and right found, nodes are in separate branches
-        if(left && right){ return node; }
-        //Otherwise, check if left/right subtree is LCA
-        return left? left : right;
-      }
-      return recurse(this, a, b).value;
-    }
-    else {return null}
-  }
-
-
-  reverse(){
-    var reversedTree = new BinaryTreeNode(this.value);
-    function recurse(node, reversedNode){
-      if(node === null){return null}
-      if(node.left){
-        reversedNode.insertRight(node.left.value);
-        recurse(node.left, reversedNode.right)
-      }
-      if(node.right){
-        reversedNode.insertLeft(node.right.value);
-        recurse(node.right, reversedNode.left)
-      }
-    }
-    recurse(this, reversedTree);
-    return reversedTree;
-  }
-
-
 }
 
 module.exports = BinaryTree;
--- tests/binaryTreeTests.js
+++ tests/binaryTreeTests.js
@@ -3,48 +3,81 @@
 var BinaryTree = require('../index.js');
 var assert = require('assert');
 
-
 /*  Test tree is a replica of the tree found in wikipedia under:
  *  https://en.wikipedia.org/wiki/Tree_traversal
+                 F
+              B     G
+            A   D     I
+               C E     H
  */
-var testTree = new BinaryTree('F')
-let bNode = testTree.appendChildNode('B', 'left', testTree.root);
-let aNode = testTree.appendChildNode('A', 'left', bNode);
-let dNode = testTree.appendChildNode('D', 'right', bNode);
-let cNode = testTree.appendChildNode('C', 'left', dNode);
-let eNode = testTree.appendChildNode('E', 'right', dNode);
-let gNode = testTree.appendChildNode('G', 'right', testTree.root);
-let iNode = testTree.appendChildNode('I', 'right', gNode);
-let hNode = testTree.appendChildNode('H', 'left', iNode);
+var alphabetTree = new BinaryTree('F')
+let bNode = alphabetTree.appendChildNode('B', 'left', alphabetTree.root);
+let aNode = alphabetTree.appendChildNode('A', 'left', bNode);
+let dNode = alphabetTree.appendChildNode('D', 'right', bNode);
+let cNode = alphabetTree.appendChildNode('C', 'left', dNode);
+let eNode = alphabetTree.appendChildNode('E', 'right', dNode);
+let gNode = alphabetTree.appendChildNode('G', 'right', alphabetTree.root);
+let iNode = alphabetTree.appendChildNode('I', 'right', gNode);
+let hNode = alphabetTree.appendChildNode('H', 'left', iNode);
 
 //Correct outputs
 const CORRECT_DSF_PREORDER = ['F','B','A','D','C','E','G','I','H'];
 const CORRECT_DSF_INORDER = ['A','B','C','D','E','F','G','H','I'];
 const CORRECT_DSF_POSTORDER = ['A','C','E','D','B','H','I','G','F'];
 const CORRECT_BSF = ['F','B','G','A','D','I','C','E','H'];
+const CORRECT_LEAF_DEPTHS = [ 2, 3, 3, 3 ];
+const CORRECT_A_PATH = ['F','B'];
+const CORRECT_A1_PATH = ['F','B', 'A'];
+const CORRECT_E_PATH = ['F','B','D'];
+const CORRECT_H_PATH = ['F','G','I'];
 
 
 describe('BinaryTree', function() {
 
   describe('#DSF()', function() {
     it('should return an array of pre-ordered node values', function() {
-      assert.deepStrictEqual(testTree.DSF(),CORRECT_DSF_PREORDER);
-      assert.deepStrictEqual(testTree.DSF('PREORDER'),CORRECT_DSF_PREORDER);
+      assert.deepStrictEqual(alphabetTree.DSF('PREORDER'),CORRECT_DSF_PREORDER);
     });
     it('should return an array of in-ordered node values', function() {
-      assert.deepStrictEqual(testTree.DSF('INORDER'),CORRECT_DSF_INORDER);
+      assert.deepStrictEqual(alphabetTree.DSF(),CORRECT_DSF_INORDER);
+      assert.deepStrictEqual(alphabetTree.DSF('INORDER'),CORRECT_DSF_INORDER);
     });
     it('should return an array of post-ordered node values', function() {
-      assert.deepStrictEqual(testTree.DSF('POSTORDER'),CORRECT_DSF_POSTORDER);
+      assert.deepStrictEqual(alphabetTree.DSF('POSTORDER'),CORRECT_DSF_POSTORDER);
     });
   });
-
 
   describe('#BSF()', function() {
     it('should return an array of node values by breath order', function() {
-      assert.deepStrictEqual(testTree.BSF(),CORRECT_BSF);
+      assert.deepStrictEqual(alphabetTree.BSF(),CORRECT_BSF);
     });
   });
 
+  describe('#getNodeByValue()', function() {
+    it('should return the first instance of the node with matching value', function() {
+      assert.deepStrictEqual(alphabetTree.getNodeByValue('A'), aNode );
+      //assert.deepStrictEqual(alphabetTreeWithRepetitions.getNodeByValue('A', 2), a2Node );
+    });
+  });
 
+  describe('#findPathToValue()', function() {
+    it('should return the path to the passed value', function() {
+      assert.deepStrictEqual(alphabetTree.findPathToValue('A'),CORRECT_A_PATH);
+      //assert.deepStrictEqual(alphabetTreeWithRepetitions.findPathToValue('A', 2),CORRECT_A1_PATH);
+    });
+  });
+
+  describe('#findPathToNode()', function() {
+    it('should return the path to the passed value', function() {
+      assert.deepStrictEqual(alphabetTree.findPathToNode(aNode),CORRECT_A_PATH);
+      //assert.deepStrictEqual(alphabetTreeWithRepetitions.findPathToNode(a2Node),CORRECT_A1_PATH);
+    });
+  });
+
+  describe('#getLeafDepths()', function() {
+    it('should return an array of leaf depths', function() {
+      assert.deepStrictEqual(alphabetTree.getLeafDepths(), CORRECT_LEAF_DEPTHS);
+    });
+  });
 });
+
